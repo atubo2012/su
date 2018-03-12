4dc37308e7aa6058e964da3b0ca553ad9f2ca270
Sean Huang <sean.wang.cn@gmail.com>
2018-03-12 23:00:02 +0800

    feat: 增加系统服务列表的展示

--- sysinfo.sh
+++ sysinfo.sh
@@ -6,7 +6,8 @@ echo "====第一行应用服务器、第二行数据服务器===="
 cat /etc/hosts  
 
 echo "====操作系统信息===="
-lsb_release -a
+#lsb_release -a
+cat /proc/version
 echo "====内核配置信息===="
 uname -r
 echo "====系统字长信息===="
@@ -26,4 +27,10 @@ java -version
 
 echo "====node版本信息===="
 node --version
+
+echo "====服务信息===="
+systemctl list-units --type=service
+#systemctl list-units --type=target
+#systemctl list-units --type=socket
+
 
