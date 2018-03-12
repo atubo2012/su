596a4a18badf952a3ca112a6d106300e22cbf8d2
Sean Huang <sean.wang.cn@gmail.com>
2018-03-12 22:53:51 +0800

    feat:将清理周期以参数方式保存到setsecinfo.sh

--- cleanlog.sh
+++ cleanlog.sh
@@ -3,6 +3,7 @@
 #PM2 log
 
 #dc log
-find $DC_HOME/log/ -mtime 7 -name "*.log" -exec rm -rf {} \; >>  $DC_HOME/log/clean.log 2>&1 &
-find $DC_HOME/log/ -mtime 7 -name "*.html" -exec rm -rf {} \;  >>  $DC_HOME/log/clean.log 2>&1 &
+find $DC_HOME/log/ -mtime +$SI_PARM_DATAKEEPDAYS -name "*.log" -exec rm -rf {} \; >>  $DC_HOME/log/clean.log  &
+find $DC_HOME/log/ -mtime +$SI_PARM_DATAKEEPDAYS -name "*.html" -exec rm -rf {} \;  >>  $DC_HOME/log/clean.log  &
+find $DC_HOME/data/ -mtime +$SI_PARM_DATAKEEPDAYS -name "*.xlsx" -exec rm -rf {} \;  >>  $DC_HOME/log/clean.log 2>&1 &
 echo `date`>> $DC_HOME/log/clean.log
