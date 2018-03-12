6737937902cbd9b951817ce704b91df6316e4fe2
Sean Huang <sean.wang.cn@gmail.com>
2018-03-12 22:58:02 +0800

    feat: 增加HOME类环境变量设置规范的说明，增加IP地址环境变量

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,13 @@
 #设置环境变量的专用脚本，不要在.bashrc中设置，目的是复用脚本
+#顺序:系统软件的HOME、应用的HOME
+
 #export NODE_HOME=$HOME/download/node-v8.9.1-linux-x64
 export NODE_HOME=$HOME/download/node-v6.11.0-linux-x64
 export PATH=$NODE_HOME/bin:$PATH
+
+
+export DC_HOME=$HOME/workspace/nodejs/cheerio
+export ZH_HOME=$HOME/workspace/zhaogong-be
+
+export LANG=en_US.UTF-8
+export IP=`/sbin/ifconfig eth0 2>/dev/null | awk '/inet/ {print $2}' | sed 's/addr://'`
