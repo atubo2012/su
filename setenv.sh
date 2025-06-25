bad907e6e8e4b823f9d10592942e6691da0c7b7e
Sean Wang <sean.wang.cn@gmail.com>
2025-06-25 15:18:36 -0700

    remove configuration for app

--- setenv.sh
+++ setenv.sh
@@ -1,13 +1,7 @@
-#设置环境变量的专用脚本，不要在.bashrc中设置，目的是复用脚本
-#顺序:系统软件的HOME、应用的HOME
-
-#export NODE_HOME=$HOME/download/node-v8.9.1-linux-x64
 export NODE_HOME=$HOME/download/node-v6.11.0-linux-x64
 export PATH=$NODE_HOME/bin:$PATH
 
 
-export DC_HOME=$HOME/workspace/nodejs/cheerio
-export ZH_HOME=$HOME/workspace/zhaogong-be
 export SYSMONITOR_HOME=/var/log/sysmonitor
 
 export LANG=en_US.UTF-8
